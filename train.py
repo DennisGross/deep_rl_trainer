db124f5cca17d9ac515e331629e5a06068ef8ed1
Dennis <dgross@science.ru.nl>
2022-05-13 10:08:07 +0200

    New Status

--- train.py
+++ train.py
@@ -16,15 +16,18 @@ if __name__ == "__main__":
             "evaluation_interval": command_line_arguments['eval_interval'],
             "num_workers": command_line_arguments['num_workers'],
             "num_envs_per_worker": command_line_arguments['num_envs_per_worker'],
+            #"num_gpus": 0,
+            #"num_cpus": 1,
             'model': {
                 'fcnet_hiddens' : [100, 100],
                 'fcnet_activation' : "tanh"
             },
+            
             "env_config": parse_env_config(command_line_arguments['env_config'])
             #"lr": tune.grid_search([0.01, 0.001, 0.0001]),
         }
     config["env"], _ = create_env(command_line_arguments['env'], command_line_arguments['env_config'])
-
+    print(command_line_arguments)
     analysis = tune.run(
         command_line_arguments['rl'],
         local_dir=TUNE_DIR,
@@ -33,6 +36,6 @@ if __name__ == "__main__":
         restore=get_checkpoint_path(command_line_arguments['run_id'], command_line_arguments['tracking_uri']),
         stop={'episode_reward_mean': command_line_arguments['reward_threshold']},
         config=config,
-        callbacks=[MLflowLoggerCallback(save_artifact=True, tracking_uri=command_line_arguments['tracking_uri'])]
+        callbacks=[MLflowLoggerCallback(save_artifact=True, experiment_name='Default', tracking_uri=command_line_arguments['tracking_uri'])]
     )
     clean_up()
